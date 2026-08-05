66ca2c5cfdb898cf31114b9e25e41e4fa9eaac31
Mauro Olivo <olivomauro@gmail.com>
2026-08-05 15:56:08 +0200

    feat: enhance Tab component with button for exiting the tab; refactor TabLayout for better readability

--- src/app/navigation/alpha/(tabs)/index.tsx
+++ src/app/navigation/alpha/(tabs)/index.tsx
@@ -1,9 +1,25 @@
+import { Button } from "@/components/ui";
+import { useRouter } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
-
 export default function Tab() {
+  const router = useRouter();
   return (
     <View style={styles.container}>
       <Text>Tab Home</Text>
+      <Button
+        className="mt-lg"
+        fullWidth={false}
+        size="md"
+        onPress={() => {
+          if (router.canGoBack()) {
+            router.back();
+            return;
+          }
+          router.replace("/");
+        }}
+      >
+        Exit this tab
+      </Button>
     </View>
   );
 }
